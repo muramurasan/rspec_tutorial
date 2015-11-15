ee51abed947ff7758ef028298c438f08240013e9
MATSUMURAYASUHIRO <ym@MATSUMURAYASUHIRO-no-MacBook-Air.local>
2015-11-16 01:22:24 +0900

    Use subject

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,10 +1,11 @@
 require_relative 'message_filter'
 
-describe MessageFilter do
+describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
+  subject { @filter }
 
-  it { expect(@filter).to be_detect('hello from foo') }
-  it { expect(@filter).not_to be_detect('hello, world!') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world!') }
 end
